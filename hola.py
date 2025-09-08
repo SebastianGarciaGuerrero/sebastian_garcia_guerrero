be393a059e474801660fa4c1e7aa8dde3d1dc21e
SebastianGarciaGuerrero <seba.garcia.g@gmail.com>
2025-09-08 09:12:49 -0300

    primer commit

---
+++ hola.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Función que se ejecuta al presionar el botón
+def mostrar_mensaje():
+    messagebox.showinfo("Saludo", "Hola")
+
+# Ventana principal
+ventana = tk.Tk()
+ventana.title("Ejemplo botón")
+ventana.geometry("200x100")
+
+# Botón
+boton = tk.Button(ventana, text="Presionar", command=mostrar_mensaje)
+boton.pack(pady=20)
+
+# Bucle principal
+ventana.mainloop()
